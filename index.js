d0141c397d70db7705b210f392bd876b64072741
frankiensteinne <nobleza.frankie@gmail.com>
2023-08-29 22:06:23 +0800

    Added html and js files

---
+++ index.js
@@ -0,0 +1,8 @@
+const box = document.createElement("div");
+box.id = "box";
+document.body.appendChild(box);
+
+const button = document.createElement("button");
+button.innerText = "Button";
+button.id = "button-1"; 
+
