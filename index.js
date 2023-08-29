01248d21d62f3b8705d341e94eb2531a6f0ce828
frankiensteinne <nobleza.frankie@gmail.com>
2023-08-30 00:34:56 +0800

    added basic weeks calculator

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 /* DOM VARIABLES */
-const calculateWeeksBtn = document.querySelector("#calculateWeeks");
+const calculateWeeksBtn = document.querySelector("#calculateWeeks")
 
-const designRate = document.querySelector("#designRate").value;
-const devRate = document.querySelector("#development-rate").value;
+let noOfWeeks = document.getElementById("numberOfWeeks")
+let designRate = document.getElementById("designRate")
+let devRate = document.getElementById("developmentRate")
+let depRate = document.getElementById("deploymentRate")
+let ktRate = document.getElementById("ktRate")
 
+let noOfDesign = document.getElementById("numberOfDesign")
+let noOfDev = document.getElementById("numberOfDevelopment")
+let noOfDep = document.getElementById("numberOfDeployment")
+let noOfKt = document.getElementById("numberOfKt")
 
-calculateWeeksBtn.addEventListener("click",myFunction);
-
-function myFunction() {
-  console.log(designRate+0, typeof designRate)
-  console.log(document.getElementById("#designRate"))
-  alert (designRate);
+const calculateWeeks = () => {
+  let noOfWeeksNum = Number(noOfWeeks.value)
+  let weekTracker = Math.ceil(noOfWeeksNum/4)
+  console.log(weekTracker, noOfWeeksNum)
+  let startOfTracker = 1
+  for (i=1; i<= weekTracker; i+=1) {
+    console.log(`week ${startOfTracker}`)
+    startOfTracker += 4
+  }
 }
+
+calculateWeeksBtn.addEventListener("click",calculateWeeks);
