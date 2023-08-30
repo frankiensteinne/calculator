ad08481d8056b49d17680b0373d59f3381575c0f
frankiensteinne <nobleza.frankie@gmail.com>
2023-08-30 14:33:56 +0800

    removed payables button, trandformed to updated upon input

--- index.js
+++ index.js
@@ -1,50 +1,92 @@
 /* DOM VARIABLES */
-const calculateBtn = document.querySelector("#calculate")
-let noOfWeeks = document.getElementById("numberOfWeeks")
+const calculateBtn = document.querySelector("#calculate");
+const noOfWeeks = document.getElementById("numberOfWeeks");
 
-let designRate = document.getElementById("designRate")
-let devRate = document.getElementById("developmentRate")
-let depRate = document.getElementById("deploymentRate")
-let ktRate = document.getElementById("ktRate")
+const designRate = document.getElementById("designRate");
+const devRate = document.getElementById("developmentRate");
+const depRate = document.getElementById("deploymentRate");
+const ktRate = document.getElementById("ktRate");
 
-let noOfDesign = document.getElementById("numberOfDesign")
-let noOfDev = document.getElementById("numberOfDevelopment")
-let noOfDep = document.getElementById("numberOfDeployment")
-let noOfKt = document.getElementById("numberOfKt")
+const noOfDesign = document.getElementById("numberOfDesign");
+const noOfDev = document.getElementById("numberOfDevelopment");
+const noOfDep = document.getElementById("numberOfDeployment");
+const noOfKt = document.getElementById("numberOfKt");
 
-let totalValueCont=  document.getElementById("totalValue")
-let otherHalf = document.getElementById("otherHalf")
+const totalValueCont = document.getElementById("totalValue");
+const otherHalf = document.getElementById("otherHalf");
+const newBtn = document.createElement("button");
 
-/* FUNCTIONS */
 
-/* Calculates total value of sprint and create
-element blocks per billing period */
 const calculate = () => {
-  total()
-  let noOfWeeksNum = Number(noOfWeeks.value)
-  let weekTracker = Math.ceil(noOfWeeksNum/4+1)
-  let startOfTracker = 1
-  for (i=1; i<= weekTracker; i+=1) {
-    let newHeading = document.createElement("h2")
-    newHeading.innerHTML=`Week ${startOfTracker}`
-    otherHalf.appendChild(newHeading)
-    startOfTracker += 4
+  totalValueCont.innerHTML = "";
+
+  const designValue = Number(designRate.value) * Number(noOfDesign.value);
+  const devValue = Number(devRate.value) * Number(noOfDev.value);
+  const depValue = Number(depRate.value) * Number(noOfDep.value);
+  const ktValue = Number(ktRate.value) * Number(noOfKt.value);
+  const overAll = designValue + devValue + depValue + ktValue;
+
+  const newHeading = document.createElement("h2");
+  newHeading.innerHTML = `Total: PHP ${overAll.toFixed(2)}`;
+  totalValueCont.appendChild(newHeading);
+
+  const noOfWeeksNum = Number(noOfWeeks.value);
+  const weekTracker = Math.ceil(noOfWeeksNum / 4 + 1);
+  let startOfTracker = 1;
+
+  for (let i = 1; i <= weekTracker; i += 1) {
+    const newHeading = document.createElement("h2");
+    newHeading.innerHTML = `Week ${startOfTracker}`;
+    otherHalf.appendChild(newHeading);
+
+    const newContainer = document.createElement("div");
+    otherHalf.appendChild(newContainer);
+
+    if (startOfTracker === 1) {
+      newContainer.innerHTML = `<h3>Amount: PHP ${(overAll / 2).toFixed(2)}</h3>`;
+    } else {
+      newContainer.innerHTML = `
+        <div class="wrapper">
+          <div class="payablesResult"><h3>PHP 0.00</h3></div>
+          <label for="numDesign">Number of Design Sprints</label>
+          <input type="text" name="numDesign" class="numDesign newInputEl"><br>
+          <label for="numDev">Number of Development Sprints</label>
+          <input type="text" name="numDev" class="numDev newInputEl"><br>
+          <label for="numDep">Number of Deployment Activities</label>
+          <input type="text" name="numDep" class="numDep newInputEl"><br>
+          <label for="numKT">Number of KT Sessions</label>
+          <input type="text" name="numKT" class="numKT newInputEl"><br>
+        </div>
+      `;
+
+      // Attach input event listeners to calculate payables when input values change
+      const inputFields = newContainer.querySelectorAll("input.newInputEl");
+      inputFields.forEach(input => {
+        input.addEventListener("input", () => {
+          updatePayables(newContainer);
+        });
+      });
+    }
+
+    startOfTracker += 4;
   }
-}
-const total = () => {
-  let designValue = Number(designRate.value)*Number(noOfDesign.value)
-  let devValue = Number(devRate.value)*Number(noOfDev.value)
-  let depValue = Number(depRate.value)*Number(noOfDep.value)
-  let ktValue = Number(ktRate.value)*Number(noOfKt.value)
-  let overAll = designValue + devValue + depValue + ktValue
-  let newHeading = document.createElement("h2")
-  newHeading.innerHTML=`Total: PHP ${overAll}.00`
-  totalValueCont.appendChild(newHeading)
-}
+};
 
-// const addElements = () => {
-//   const newContainer = document.createElement("div")
-//   const
-// }
+const updatePayables = (container) => {
+  const numDesignValue = Number(container.querySelector(".numDesign").value);
+  const numDevValue = Number(container.querySelector(".numDev").value);
+  const numDepValue = Number(container.querySelector(".numDep").value);
+  const numKtValue = Number(container.querySelector(".numKT").value);
 
-calculateBtn.addEventListener("click",calculate);
+  const payables =
+    (numDesignValue * Number(designRate.value) +
+      numDevValue * Number(devRate.value) +
+      numDepValue * Number(depRate.value) +
+      numKtValue * Number(ktRate.value)) /
+    2;
+
+  const payablesText = `Amount: PHP ${payables.toFixed(2)}`;
+  container.querySelector(".payablesResult").innerHTML = `<h3>${payablesText}</h3>`;
+};
+
+calculateBtn.addEventListener("click", calculate);
